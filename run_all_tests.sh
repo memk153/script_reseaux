ba21167930a8389ad2cb8a98d51c59d5d0ee38cb
borisnoah <borisnoah91pgmail.com>
2025-05-12 11:19:26 +0100

    Ajout du script global run_all_tests.sh

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,10 +1,10 @@
 echo  "==== Diagnostic reseau complet ===="
-echo " 1 verification de la passerelle..."
+echo "  verification de la passerelle..."
 bash ping-check.sh
-echo "2 scan de ports sur 127.0.0.1..."
+echo " scan de ports sur 127.0.0.1..."
 bash port-scan.sh 127.0.0.1
 
-echo " 3 log de bande passante..."
+echo "  log de bande passante..."
 bash bandwidth-log.sh
 
 echo "==== fin du diagnostic ===="
